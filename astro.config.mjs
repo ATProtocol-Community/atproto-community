fa8cb42ac8c707a8f2de954fda7e758ed1d54a9e
Boris Mann <boris@bmannconsulting.com>
2026-04-17 13:24:03 -0700

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atprotocol-community.github.io',
-  base: '/atproto-community',
+  site: 'https://atmosphere.community',
+  base: '/',
   output: 'static',
 });
