7d48e90be60e4a3b645c730e290b8f3d43087b42
Brittany Ellich <brittanyellich@github.com>
2026-04-16 22:06:33 -0700

    Update site for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atmosphere.community',
+  site: 'https://atprotocol-community.github.io',
+  base: '/atproto-community',
   output: 'static',
 });
